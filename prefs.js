120085e7f7634f5620d36608db17427441524b60
pancake <pancake@nopcode.org>
2017-08-01 13:58:09 +0200

    Better settings

--- prefs.js
+++ prefs.js
@@ -5,6 +5,8 @@ const electron = require('electron');
 const dialogs = require('dialogs');
 const {Menu, MenuItem, getCurrentWindow} = electron.remote;
 
+// getCurrentWindow().webContents.openDevTools();
+
 // getCurrentWindow().setSize(400, 300, false);
 electron.webFrame.setVisualZoomLevelLimits(1, 1);
 electron.webFrame.setLayoutZoomLevelLimits(0, 0);
@@ -14,7 +16,7 @@ var prependScreen = false;
 var filterWord = '';
 
 document.addEventListener('DOMContentLoaded', function () {
-  electron.ipcRenderer.send('run-command', 'ej');
+  electron.ipcRenderer.send('run-command', 'ej|');
 
   document.ondragover = (ev) => {
     ev.preventDefault();
@@ -24,16 +26,20 @@ document.addEventListener('DOMContentLoaded', function () {
   };
 
   electron.ipcRenderer.on('command-output', (event, arg) => {
+try {
     const obj = JSON.parse(arg.result);
     switch (arg.command) {
-      case 'ej':
-  //    alert(obj['asm.arch']);
+      case 'ej|':
+      //   alert(obj['asm.arch']);
    //   alert(obj['asm.bits']);
         break;
       default:
         alert(JSON.stringify(obj));
         break;
     }
+} catch (e) {
+alert (arg.result);
+}
   });
 
   // boilerplate for tabs
@@ -47,31 +53,48 @@ document.addEventListener('DOMContentLoaded', function () {
   }
   function resetTabs (but) {
     removeClass($('general-tab'), 'active');
-    removeClass($('colors-tab'), 'active');
+    // removeClass($('colors-tab'), 'active');
     removeClass($('analysis-tab'), 'active');
-    removeClass($('project-tab'), 'active');
+    removeClass($('debugger-tab'), 'active');
+    removeClass($('disasm-tab'), 'active');
+ //   removeClass($('project-tab'), 'active');
     $('general-div').style.visibility = 'hidden';
-    $('colors-div').style.visibility = 'hidden';
+    // $('colors-div').style.visibility = 'hidden';
     $('analysis-div').style.visibility = 'hidden';
-    $('project-div').style.visibility = 'hidden';
+    $('debugger-div').style.visibility = 'hidden';
+    $('disasm-div').style.visibility = 'hidden';
+    // $('project-div').style.visibility = 'hidden';
     if (but) {
       addClass($(but), 'active');
       $(but.replace('-tab', '-div')).style.visibility = 'visible';
     }
   }
+/*
   onclick('check-button', _ => {
     getCurrentWindow().close();
   });
+*/
   onclick('general-tab', _ => {
     resetTabs('general-tab');
   });
+/*
   onclick('colors-tab', _ => {
     resetTabs('colors-tab');
   });
+*/
   onclick('analysis-tab', _ => {
     resetTabs('analysis-tab');
   });
+  onclick('debugger-tab', _ => {
+    resetTabs('debugger-tab');
+  });
+  onclick('disasm-tab', _ => {
+    resetTabs('disasm-tab');
+  });
+/*
   onclick('project-tab', _ => {
     resetTabs('project-tab');
   });
+*/
+  resetTabs('general-tab');
 });
